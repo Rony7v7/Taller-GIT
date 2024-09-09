d0a8029afb8659db3b54471d4a98ce9836cfc65e
JuanJDlp <juanjosedelapava1@gmail.com>
2024-09-09 17:02:53 -0500

    feature: modified filtro de mayor o menor of vehicles

--- code/main.py
+++ code/main.py
@@ -10,30 +10,38 @@ class Main:
         """
         self.vehicle_list = []
 
-    def add_vehicle(self, Vehicle):
+    def add_vehicle(self, vehicle):
         """
         Agrega un vehículo a la lista.
 
         :param vehicle: Objeto de tipo Vehicle.
         :type vehicle: Vehicle
         """
-        self.vehicle_list.append(Vehicle)
+        self.vehicle_list.append(vehicle)
 
-    def find_vehicles_by_year(self, year):
+    def find_vehicles_by_year(self, year, mayor_o_menor):
         """
         Busca vehículos por su año de fabricación.
 
         :param year: Año de fabricación del vehículo.
         :type year: int
-        :return: Lista de vehículos fabricados en el año especificado.
+        :param mayor_o_menor: Especifica si el filtro es para vehículos mayores o menores al año especificado.
+        :type mayor_o_menor: str
+        :return: Lista de vehículos que cumplen con el criterio de año.
         :rtype: list
         """
-        vehicles_by_year = [Vehicle for Vehicle in self.vehicle_list if vehicle.year == year]
+        if mayor_o_menor == 'mayor':
+            vehicles_by_year = [vehicle for vehicle in self.vehicle_list if vehicle.year > year]
+        elif mayor_o_menor == 'menor':
+            vehicles_by_year = [vehicle for vehicle in self.vehicle_list if vehicle.year < year]
+        else:
+            raise ValueError("El parámetro 'mayor_o_menor' debe ser 'mayor' o 'menor'.")
         return vehicles_by_year
 
     def print_vehicle_list(self):
         """
         Imprime la lista de vehículos con sus características.
         """
-        for Vehicle in self.vehicle_list:
-            print(f"Marca: {Vehicle.brand}, Modelo: {Vehicle.model}, Año: {Vehicle.year}")
+        for vehicle in self.vehicle_list:
+            print(f"Marca: {vehicle.brand}, Modelo: {vehicle.model}, Año: {vehicle.year}")
+
